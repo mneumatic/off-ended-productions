93507f740c11044833d0b41cebb18975994abc07
mneumatic <mneumatic@icloud.com>
2024-12-13 18:28:41 -0800

    added promise

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 (() => {
-  import('/js/components/navbar')
-  import('/js/components/footbar')
+  Promise.all([
+    import('/js/components/navbar'),
+    import('/js/components/footbar')
+  ]).then(() => {
+    console.log('Web Components Loaded.')
+  })
 })()
