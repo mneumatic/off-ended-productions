85942586f1a8574e9738f859634190265f09d693
mneumatic <mneumatic@icloud.com>
2024-12-13 18:42:59 -0800

    fix for pathing of imports

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-(() => {
-  Promise.all([
-    import('./../js/components/navbar'),
-    import('./../js/components/footbar')
-  ]).then(() => {
-    console.log('Web Components Loaded.')
-  })
-})()
+Promise.all([
+  import('./../js/components/navbar'),
+  import('./../js/components/footbar')
+]).then(() => {
+  console.log('Web Components Loaded.')
+})
