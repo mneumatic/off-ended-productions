1fb20607230cb34ae43d8bcce3c7e1ce54554e98
mneumatic <mneumatic@icloud.com>
2024-12-15 09:06:53 -0800

    fixed / suppressed issues

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,5 @@
+// noinspection JSCheckFunctionSignatures
+
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
