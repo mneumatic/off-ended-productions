8d3698619039c2e901e20d8a4df87832a0b9244c
mneumatic <mneumatic@icloud.com>
2024-12-21 20:55:38 -0800

    image animations using Observer API

--- js/images.js
+++ js/images.js
@@ -1,22 +1,36 @@
-function showGroupImages(section, array) {
-  window.addEventListener('scroll', () => {
-    if (isInViewport(section) && !array[0].classList.contains('show-image')) {
-      showPodImages(array)
+const options = {
+  root: null,
+  threshold: 0.50,
+  rootMargin: '0px'
+}
+
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Element is in view, do something
+      const elements = entry.target.children
+      let array = []
+
+      if (elements.length > 2) {
+        for (let i = 0; i < elements.length; i++) {
+          if (elements[i].tagName === 'IMG') {
+            array.push(elements[i])
+          } else {
+            array.push(elements[i].querySelector('img'))
+          }
+        }
+        showImages(array)
+      } else {
+        slideImage(elements[0])
+      }
+
+      // Optionally stop observing the element
+      observer.unobserve(entry.target)
     }
-  });
+  })
 }
 
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function showPodImages (element) {
+function showImages (element) {
   element[0].classList.add("show-image")
 
   setTimeout(function () {
@@ -27,3 +41,30 @@ function showPodImages (element) {
     element[2].classList.add("show-image")
   }, 600);
 }
+
+function slideImage (element) {
+  element.classList.add("slide-in-image")
+}
+
+const observer = new IntersectionObserver(handleIntersection, options);
+
+const podcastImages = document.querySelector("#podcast .img-container")
+const musicImages = document.querySelector("#music .img-container")
+
+const aboutImage1 = document.querySelector("#about .twojz")
+const aboutImage2 = document.querySelector("#about .mm")
+const aboutImage3 = document.querySelector("#about .mo")
+
+const PSImages = document.querySelector("#platinum-signatures .ps-images")
+const communityImages = document.querySelector("#community .community-images")
+
+observer.observe(podcastImages)
+observer.observe(musicImages)
+
+observer.observe(aboutImage1)
+observer.observe(aboutImage2)
+observer.observe(aboutImage3)
+
+observer.observe(PSImages)
+observer.observe(communityImages)
+
